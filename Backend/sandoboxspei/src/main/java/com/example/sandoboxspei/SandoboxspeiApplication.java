074dcc595c1b4bf01fa068089792347bebaae73b
GAreeem <20223tn084@utez.edu.mx>
2026-09-25 11:38:40 -0600

    Inicializar proyecto Spring Boot

---
+++ Backend/sandoboxspei/src/main/java/com/example/sandoboxspei/SandoboxspeiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandoboxspei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandoboxspeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandoboxspeiApplication.class, args);
+	}
+
+}
---
+++ Backend/sandoboxspei/src/test/java/com/example/sandoboxspei/SandoboxspeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandoboxspei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandoboxspeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
